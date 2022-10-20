d69df6de38549cda704405a055437774b8478094
Tobias Gingerich <78279787+duckinmyhat@users.noreply.github.com>
2022-10-20 07:57:17 -0800

    sus

---
+++ 1.2.1.py
@@ -0,0 +1,15 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trt
+import random as rnd 
+
+#-----game configuration----
+trt.color('yellow')
+trt.shapesize
+#-----initialize turtle-----
+
+
+#-----game functions--------
+
+
+#-----events----------------
