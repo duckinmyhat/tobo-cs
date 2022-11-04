9ec29d0fbe9f4dd76a5fa87b868e1f86f1154c3c
Tobias Gingerich <78279787+duckinmyhat@users.noreply.github.com>
2022-11-04 08:36:41 -0800

    nooo

--- saveus.1.2.1.py
+++ saveus.1.2.1.py
@@ -1,12 +1,12 @@
 #-----import statements-----
 import turtle as trt
 import random as rnd
-import leaderboard as lb
+#import leaderboard as lb
 
 
 #-----countdown variables-----
 font_setup = ("Arial", 20, "normal")
-timer = 30
+timer = 5
 counter_interval = 1000   #1000 represents 1 second
 timer_up = False
 #name call
@@ -14,6 +14,27 @@ timer_up = False
 # leaderboard variables
 leaderboard_file_name = "a122_leaderboard.txt"
 player_name = input('what is your name\n')
+
+# Add this function to your game code
+
+# manages the leaderboard for top 5 scorers
+def manage_leaderboard():
+
+  global score
+  global spot
+
+  # get the names and scores from the leaderboard file
+  leader_names_list = lb.get_names(leaderboard_file_name)
+  leader_scores_list = lb.get_scores(leaderboard_file_name)
+
+  # show the leaderboard with or without the current player
+  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
+
+  else:
+    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
+
 #-----game configuration----
 turtle_color = 'pink'
 turtle_shape = 'turtle'
@@ -63,6 +84,7 @@ def countdown():
     counter.write("Time's Up", font=font_setup)
     timer_up = True
     whack_me.hideturtle()
+    manage_leaderboard()
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
