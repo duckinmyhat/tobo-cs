801210c0bd7a7b22b57c8ff492ed058c2176527d
tobo ducko <78279787+duckinmyhat@users.noreply.github.com>
2022-11-22 07:42:51 -0900

    ah

--- apple avalavhssndfs/123-Apple-Avalanche-TobiasGingeric1/1.4./1.2.4.py
+++ apple avalavhssndfs/123-Apple-Avalanche-TobiasGingeric1/1.4./1.2.4.py
@@ -1,11 +1,20 @@
 import turtle as t 
-x = 100
-t.right(90)
+x = 0
+t.left(90)
 for p in range (17):
-    t.forward(x)
-    t.right(90)
-    x -= 5
+    #t.forward(x)
+    
 
+    x += 5
+    if (x > 5):
+        t.forward(10)
+        t.penup()
+        t.forward(10)
+        t.pendown()
+        t.forward(x-10)
+    else:
+        t.forward(10+x)
+    t.left(90)
 
 wn = t.Screen()
 wn.mainloop()
