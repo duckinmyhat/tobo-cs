a644dfb3c93baa545fc3ef86c4333e80e60ea118
tobo ducko <78279787+duckinmyhat@users.noreply.github.com>
2022-11-27 17:13:57 -0900

    1.2.4 done ???????

--- 1.2.4.py
+++ 1.2.4.py
@@ -1,21 +1,40 @@
 import turtle as t 
+import random as rnd
 
-x = 0
+path= 5 
+l = path * 2
+
+
+
+
 t.left(90)
 for p in range (17):
-    #t.forward(x)
-    
 
-    x += 5
-    if (x > 5):
-        t.forward(10)
+    if (l > 15):
+        door = rnd.randint(path, l)
+        first = l - door
+        t.forward(first)
         t.penup()
-        t.forward(10)
+        t.forward(5)
         t.pendown()
-        t.forward(x-10)
-    else:
-        t.forward(10+x)
+        second = l - first - 5
+        if (second > 10):
+            wall = rnd.randint(0, second-5)
+            t.forward(wall)
+            t.left(90)
+            t.forward(path*2)
+            t.back(path*2)
+            t.right(90)
+            t.forward(second - wall)
+        else:
+            t.forward(second)   
+    else: 
+        t.forward(l)
+
     t.left(90)
+    l += path
+
+
 
 wn = t.Screen()
 wn.mainloop()
