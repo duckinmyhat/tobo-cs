c2982f8c3ea1378c1e652bc09b0e3415f45cc41c
Tobias Gingerich <78279787+duckinmyhat@users.noreply.github.com>
2023-01-10 09:58:59 -0900

    sus

--- newsem/Comp-Sci-Principle.py
+++ newsem/Comp-Sci-Principle.py
@@ -10,10 +10,14 @@ for i in range(10):
         print(add)
     x += 1
     y +=1'''
-
-string = input('imput word:')
+#quesion 3
+'''string = input('imput word:')
 num = 0
 for i in range(0, len(string)):
     
     print(string[num])
-    num += 2
+    num += 2'''
+
+#string = input('input word:')
+print(input("Input word:")[::2])
+#print(''.join([string[i] for i in range(len(string)) if i%2==0]))
