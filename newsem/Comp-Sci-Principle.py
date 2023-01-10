7f3acfe842e5e3f47386b6c6f3b60291997f2205
Tobias Gingerich <78279787+duckinmyhat@users.noreply.github.com>
2023-01-10 08:59:40 -0900

    sus

--- newsem/Comp-Sci-Principle.py
+++ newsem/Comp-Sci-Principle.py
@@ -1,3 +1,19 @@
-int_2 = 3
-int_3 = 4 
-sum = int_3 * int_2
+#question 2
+'''x = -1
+y = 0
+
+for i in range(10):
+    add = x + y
+    if add < 0:
+        print(0)
+    else:
+        print(add)
+    x += 1
+    y +=1'''
+
+string = input('imput word:')
+num = 0
+for i in range(0, len(string)):
+    
+    print(string[num])
+    num += 2
